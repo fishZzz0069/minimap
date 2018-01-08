5ff8a1d25f888c3f52b1a548447414bad1ec9d3b
Ubuntu <ubuntu@ip-172-31-23-205.us-east-2.compute.internal>
2018-01-08 16:57:43 +0000

    Solved Underscore.js compatibility problem & do the query in the fronend(still have access-control-allowl-origin problem)

--- js/index.js
+++ js/index.js
@@ -1,14 +1,36 @@
-$(function() {   
+$(function() {  
+  var DEFAULT_ZOOM = 4;
+  var GOOGLE_API_KEY= 'AIzaSyDq6a9lCfiJD2D_tUQ3GeOOv5z1wHIqHRQ';
   function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
+        var position = 
+        {
+         lat: 37.773972,
+         lng: -122.43129
+        };
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
+          zoom: DEFAULT_ZOOM , 
+          center: position
         });
         var marker = new google.maps.Marker({
-          position: uluru,
+          position: position,
           map: map
         });
-      }
-    initMap()
-})
+    
+    $.ajax({
+     url:'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+     data: {
+      'location': position.lat + ',' + position.lng,
+      'keyword':'restaurant',
+      'key':GOOGLE_API_KEY,
+      'radius':500
+      },
+     success:function(){
+           debugger;
+     },
+     fail:function(){
+           debugger;
+    }
+    });
+   }
+    initMap();
+});
