1922279df5b67d300e97485f63a4187b0879e3be
Ubuntu <ubuntu@ip-172-31-23-205.us-east-2.compute.internal>
2018-01-09 16:01:25 +0000

    implment click and show info

--- js/index.js
+++ js/index.js
@@ -2,8 +2,7 @@ $(function() {
   var DEFAULT_ZOOM = 4;
   var GOOGLE_API_KEY= 'AIzaSyDq6a9lCfiJD2D_tUQ3GeOOv5z1wHIqHRQ';
   function initMap() {
-        var position = 
-        {
+        var position =  {
          lat: 37.773972,
          lng: -122.43129
         };
@@ -11,27 +10,50 @@ $(function() {
           zoom: DEFAULT_ZOOM , 
           center: position
         });
-        var marker = new google.maps.Marker({
-          position: position,
-          map: map
-        });
-    
-    $.ajax({
-     url:'/places-info',
-     data: {
-      'lat' :position.lat,
-      'lng': position.lng,
-      'type':'restaurant',
-      'key':GOOGLE_API_KEY,
-      'radius':500
-      },
-     success:function(data){
-           debugger;
-     },
-     fail:function(err){
-           debugger;
-    }
-    });
-   }
+     
+   var datas = {
+    'location': new google.maps.LatLng(position.lat,position.lng),
+    'radius': 500,
+    'type': 'park'
+   };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(datas, function(results,status){
+     if (status === google.maps.places.PlacesServiceStatus.OK){
+      var current_infowindow;
+      _.each(results, function(place){
+           var marker = new google.maps.Marker({
+           position: {
+            'lat': place.geometry.location.lat(),
+            'lng': place.geometry.location.lng()
+      }, 
+      map:map,
+      title:place.name
+      });
+      var infowindow_content = '<div id="content">' +
+                               '<h1 id="firstHeading" class="firstHeading">' +place.name+ '</h1>' + '</div>';
+      var infowindow = new google.maps.InfoWindow({
+         content: infowindow_content
+      });
+       
+             marker.addListener('click', function(){
+                   if(current_infowindow){
+                         current_infowindow.close();
+                    }
+                   infowindow.open(map, marker);
+                   current_infowindow = infowindow;
+                
+                   showDetailedInfo(place);
+                  });
+            });
+
+      }
+     });
+   };
+
+     function showDetailedInfo(place) {
+           $('#detailed-info-wrapper').show();
+      };
+      
     initMap();
 });
