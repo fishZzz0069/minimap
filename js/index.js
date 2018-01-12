9e21eeb9c966fed615badf3f08ea5df8ae4e5b36
Ubuntu <ubuntu@ip-172-31-23-205.us-east-2.compute.internal>
2018-01-12 15:51:48 +0000

    implement search function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 $(function() {  
   var DEFAULT_ZOOM = 4;
   var GOOGLE_API_KEY= 'AIzaSyDq6a9lCfiJD2D_tUQ3GeOOv5z1wHIqHRQ';
+ 
+  var current_infowindow;
+  var markers_shown;
   function initMap() {
         var position =  {
          lat: 37.773972,
@@ -17,13 +20,51 @@ $(function() {
     'type': 'park'
    };
 
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(datas, function(results,status){
+   getNearByPlaces(map,datas);
+
+   
+    $('.place-info-visibility-toggle').on('click', function() {
+      $('#place-info-wrapper').toggleClass('visible');
+       $('#place-info-wrapper .triangle-icon').toggleClass('left');
+});
+    var search_bar = new SearchBar(function(type) {
+                 var params = {
+                  'location': new google.maps.LatLng(position.lat, position.lng),
+                  'radius': 500,
+                   'type': type
+           };
+        getNearByPlaces(map, params);
+});
+         search_bar.addTo($('body'));
+};
+function showDetailedInfo(place) {
+    var params = {
+       placeId: place['place_id']
+     };
+    service.getDetails(params, function(place) {
+      $('#hero-header-wrapper img').attr('src', place.photos[0].getUrl({'maxWidth': 408, 'maxheight': 407}));
+      $('.place-name').text(place['name']);
+      $('.place-review-score').text(place['rating']);
+      $('.place-type').text(place['types'][0]);
+      $('#place-info-wrapper').addClass('visible');
+      $('#place-info-wrapper').addClass('is-active');
+       });
+};
+  function getNearByPlaces(map, params) {
+    if(markers_shown){
+       _.each(markers_shown,function(marker){
+         marker.setMap(null);
+     });
+    }
+   markers_shown=[];
+   
+
+   service = new google.maps.places.PlacesService(map);
+   service.nearbySearch(params, function(results,status){
      if (status === google.maps.places.PlacesServiceStatus.OK){
-      var current_infowindow;
-      _.each(results, function(place){
+       _.each(results, function(place){
            var marker = new google.maps.Marker({
-           position: {
+            position: {
             'lat': place.geometry.location.lat(),
             'lng': place.geometry.location.lng()
       }, 
@@ -45,33 +86,14 @@ $(function() {
                 
                    showDetailedInfo(place);
                   });
+
+                  markers_shown.push(marker);
             });
 
       }
      });
-     $('.place-info-visibility-toggle').on('click', function() {
-      $('#place-info-wrapper').toggleClass('visible');
-              $('#place-info-wrapper .triangle-icon').toggleClass('left');
-     });
-     
-     var search_bar = new SearchBar();
-     search_bar.addTo($('body'));
-   };
 
-     function showDetailedInfo(place) {
-          var params = {
-               placeId: place['place_id']
-          };
-
-          service.getDetails(params, function(place){
-              $('#hero-header-wrapper img').attr('src', place.photos[0].getUrl({'maxWidth': 408, 'maxheight': 407}));
-              $('.place-name').text(place['name']);
-              $('.place-review-score').text(place['rating']);
-              $('.place-type').text(place['types'][0]);
-              $('#place-info-wrapper').addClass('visible');
-              $('#place-info-wrapper').addClass('is-active');
-        }); 
-    };
+    }
       
     initMap();
 });
--- js/search_bar.js
+++ js/search_bar.js
@@ -6,10 +6,17 @@ constructor(callback) {
        <input type="text" placeholder="Search mini map" class="input-text-bar" />
        <div class="search-icon"></div>
        </div>  `
+    this.callback=callback;
 }
 
 
 addTo($parent) {
         $parent.append(this.template);
+        $('.search-bar-wrapper').on('keyup',_.bind(function(e){
+          if(e.keyCode == 13){
+               this.callback($('.input-text-bar')[0].value);
+          }
+        },this));
+        
 }
 }
